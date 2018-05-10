b9a2c5388ba638b20b979e539affcf893e2456b0
js3560750 <188949420@qq.com>
2018-05-10 15:54:43 +0800

    Git push test

---
+++ src/com/js/Test.java
@@ -0,0 +1,12 @@
+package com.js;
+
+/**
+ * Author   Jinsong
+ * Email    188949420@qq.com
+ * Date     2018/5/10
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
