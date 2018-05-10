ee9b7d8c901b5c1782a332e723f9dd98fa387ec6
js3560750 <188949420@qq.com>
2018-05-10 16:24:00 +0800

    change2

--- src/com/js/Test.java
+++ src/com/js/Test.java
@@ -7,7 +7,7 @@ package com.js;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("lalalala");
+
+        System.out.println("this is adasdada ");
     }
 }
